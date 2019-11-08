520e864f496d6a0389e7cc26bd2d1f9edbbdd3af
Rishi <1990rishi@gmail.com>
2019-11-08 20:03:10 +0530

    --- Auto-Git Commit ---

--- seed-jobs/seed.groovy
+++ seed-jobs/seed.groovy
@@ -12,7 +12,7 @@ pipelineJob('studentapp-ci') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
             'url'('git@github.com:rishidadwal/jenkins.git')
-            'credentialsId'('Rishi8065')
+            
           }
         }
 
@@ -22,7 +22,7 @@ pipelineJob('studentapp-ci') {
           }
         }
       }
-      'scriptPath'('pipeline-jobs/studentapp-ci.JenkinsFile')
+      'scriptPath'('pipeline-jobs/studentapp-ci.jenkinsFile')
       'lightweight'(true)
     }
   }
